d22e2bc9f27fff6d110b6943c1f78d45451f04f2
Denys <you@example.com>
2017-09-09 21:42:04 +1200

    Initial

---
+++ src/main/java/nz/denys/reactjs/spring/ReactController.java
@@ -0,0 +1,13 @@
+package nz.denys.reactjs.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReactController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index.html";
+    }
+}
---
+++ src/main/webapp/public/app.js
@@ -0,0 +1,13 @@
+var Flight = React.createClass({
+  render: function() {
+    return (<div>employee</div>);
+  }
+});
+var FlightTable = React.createClass({
+  render: function() {
+    return (<div>employee table</div>);
+  }
+});
+ReactDOM.render(
+  <FlightTable />, document.getElementById('root')
+);
